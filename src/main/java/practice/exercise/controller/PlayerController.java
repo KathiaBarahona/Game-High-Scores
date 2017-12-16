fb0a34f20c9d0146cb343065bb8ec81b20370844
Kathia Barahona <katbara03@gmail.com>
2017-12-16 01:09:17 -0600

    Cleaning of PlayerController and Log

--- src/main/java/practice/exercise/controller/PlayerController.java
+++ src/main/java/practice/exercise/controller/PlayerController.java
@@ -1,15 +1,16 @@
 package practice.exercise.controller;
 
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import practice.exercise.entity.Player;
 import practice.exercise.exceptionHandler.IncorrectParameterException;
@@ -30,9 +32,6 @@ import practice.exercise.exceptionHandler.IncorrectPlayerException;
 import practice.exercise.exceptionHandler.ValidateError;
 import practice.exercise.exceptionHandler.ValidateErrorBuilder;
 import practice.exercise.service.PlayerService;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/players")
@@ -43,67 +42,90 @@ public class PlayerController {
 
 	@Autowired
 	private PlayerService playerService;
-
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity createPlayer(@Valid @RequestBody Optional<Player> player, Errors errors) throws URISyntaxException {
-
+	
+	
+//********************************POST REQUEST**************************************************
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<ValidateError> createPlayer(@Valid @RequestBody Optional<Player> player, Errors errors, UriComponentsBuilder ucBuilder) throws URISyntaxException {
+		LOG.info("Creating a new player...");
 		if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(ValidateErrorBuilder.fromBindingErrors(errors));
+			LOG.info("The following errors were found: {}",errors);
+			return new ResponseEntity<ValidateError>(ValidateErrorBuilder.fromBindingErrors(errors),HttpStatus.BAD_REQUEST);
         }
 		long playerId = -1;
 		if (player.isPresent()) {
 			Player p = player.get();
-			
 			p.getCategories().forEach(category-> category.setPlayer(p,false));
 			playerId = playerService.createPlayer(p);
 		}
 		player.orElseThrow(() -> new IncorrectPlayerException());
-		URI location = new URI("/players/" + playerId); 
-		return ResponseEntity.created(location).body("Player was created");
+		if(playerId != -1) {
+			LOG.info("The player {} was created",player.get());
+			HttpHeaders headers = new HttpHeaders();
+			headers.setLocation(ucBuilder.path("players/{id}").buildAndExpand(playerId).toUri());
+			return new ResponseEntity<ValidateError>(headers,HttpStatus.CREATED);
+		}
+		LOG.info("The player wasn't created");
+		return new ResponseEntity<ValidateError>(HttpStatus.BAD_REQUEST);
 	}
-
-	@RequestMapping(method = RequestMethod.PUT, value="/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity updatePlayer(@RequestBody Optional<Player> player,@RequestBody byte[] bytes,@PathVariable("id") long id, Errors errors) {
-		System.out.println(id);
-		System.out.println("Aqui revienta, no se porque");
-		System.out.println(player.get()+"-Player shit");
-		System.out.println("No deberia de imprimir");
+//*******************************************UPDATE REQUEST***************************************************
+	@RequestMapping(method = RequestMethod.PUT, value="{id}")
+	public ResponseEntity<Object> updatePlayer(@RequestBody Optional<Player> player,@PathVariable("id") long id, Errors errors) {
+		LOG.info("Player with id {} is to be updated ",id);
 		if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(ValidateErrorBuilder.fromBindingErrors(errors));
+			LOG.info("The following errors where found: "+errors);
+			return new ResponseEntity<Object>(ValidateErrorBuilder.fromBindingErrors(errors),HttpStatus.BAD_REQUEST);
         }
 		if (player.isPresent()) {
 			Player p = player.get();
 			if(Objects.isNull(p.getName()) || p.getName().equals("")) {
+				LOG.info("The player's name property is empty");
 				throw new IncorrectParameterException("name");
 			}
 			boolean updated = playerService.updatePlayer(player.get(),id); 
 			if(!updated) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Player Id not found");
+				LOG.info("Player with id {} not found.", id);
+				return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
 			}
+			Player updatedPlayer = playerService.getPlayerById(id);
+			return new ResponseEntity<Object>(updatedPlayer,HttpStatus.OK);
 		}
+		LOG.info("The informed player isn't valid");
 		player.orElseThrow(() -> new IncorrectPlayerException());
-		return ResponseEntity.ok().body("Player was updated");
+		return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
 	}
-
-	@RequestMapping(method = RequestMethod.DELETE, value="/{id}")
-	public ResponseEntity deletePlayer(@PathVariable("id") long playerId) {
+//**************************************DELETE REQUESTS****************************************************
+	@RequestMapping(method = RequestMethod.DELETE, value="{id}")
+	public ResponseEntity<Void> deletePlayer(@PathVariable("id") long playerId) {
+		LOG.info("Player with id {} is to be deleted",playerId);
 		boolean deleted = playerService.deletePlayer(playerId);
 		if(deleted) {
-			return ResponseEntity.status(HttpStatus.OK).body("Deleted.");
+			LOG.info("Player with id {} was deleted",playerId);
+			return new ResponseEntity<Void>(HttpStatus.OK);
 		}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Player Id not found");
+		LOG.info("Player with id {} not found",playerId);
+		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 	}
-
+//****************************************GET REQUESTS****************************************************
+	
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Collection<Player> getPlayersByName(@RequestParam("name") Optional<String> name) {
+	public ResponseEntity<Collection<Player>> getPlayersByName(@RequestParam("name") Optional<String> name) {
+		LOG.info("Getting players by name {}");
 		if (name.isPresent()) {
-			return playerService.getPlayersByName(name.get());
+			Collection<Player> player = playerService.getPlayersByName(name.get());
+			if(player.size() == 0) {
+				LOG.info("No player where found for name {} ",name.get());
+				return new ResponseEntity<Collection<Player>>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<Collection<Player>>(player,HttpStatus.OK);
 		}
+		LOG.info("The request is missing the name parameter.");
 		name.orElseThrow(() -> new IncorrectParameterException("name"));
-		return null;
+		return new ResponseEntity<Collection<Player>>(HttpStatus.BAD_REQUEST);
 	}
 
-	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	@RequestMapping(value = "{id}",method = RequestMethod.GET)
 	public ResponseEntity<Player> getPlayerById(@PathVariable("id") long id) {
 		LOG.info("Getting a player with id: " +id);
 		Player player = playerService.getPlayerById(id); 
@@ -115,7 +137,7 @@ public class PlayerController {
 	}
 
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/ranking")
+	@RequestMapping(method = RequestMethod.GET, value = "ranking")
 	public ResponseEntity<Collection<Player>> getTopPlayersByCategory(@RequestParam("category") Optional<String> category,
 			@RequestParam("page") Optional<Integer> page) {
 	
@@ -128,6 +150,7 @@ public class PlayerController {
 			}
 			return new ResponseEntity<Collection<Player>>(players,HttpStatus.OK);
 		}
+		LOG.info("The request is missing some parameters");
 		category.orElseThrow(() -> new IncorrectParameterException("category"));
 		page.orElseThrow(() -> new IncorrectParameterException("page"));
 		return new ResponseEntity<Collection<Player>>(HttpStatus.BAD_REQUEST);
